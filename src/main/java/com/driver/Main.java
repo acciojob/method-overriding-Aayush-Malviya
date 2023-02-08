3a821ae52c551d96900555c69d70a5526d8a29ab
Aayush-Malviya <aayushmalviya91@gmail.com>
2023-02-08 09:11:04 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    @Override
+    String meth() {
+        return "Method is overridden in Extendend class B";
+    }
+}
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.meth();
+        obj.meth();
+    }
 }
